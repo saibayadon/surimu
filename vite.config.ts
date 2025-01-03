3a2d5fde41d1b89df9770ff69ab301f80af0c8fb
Majin Bru <brunomilestone@gmail.com>
2025-01-03 23:37:00 +0100

    feat: update deps versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import type { UserConfig } from 'vite';
 
-export default defineConfig(({ mode }) => ({
-  resolve: {
-    conditions: mode === 'test' ? ['browser'] : []
-  },
-  plugins: [sveltekit()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
-    environment: 'jsdom',
-    setupFiles: ['./vitest-setup.js']
-  }
-}));
+export default defineConfig(
+  ({ mode }) =>
+    ({
+      resolve: {
+        conditions: mode === 'test' ? ['browser'] : []
+      },
+      plugins: [sveltekit()],
+      test: {
+        include: ['src/**/*.{test,spec}.{js,ts}'],
+        environment: 'jsdom',
+        setupFiles: ['./vitest-setup.js']
+      }
+    }) as UserConfig
+);
